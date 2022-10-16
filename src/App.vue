6122126119204ca6fe00ddd117140fc5e4f7d60c
3514392356@qq.com <3514392356@qq.com>
2022-10-16 16:58:51 +0800

    初始化了一下捏

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-button>有一吗</el-button>
   </div>
 </template>
 
@@ -11,13 +10,6 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped lang="less">
+
+</style>>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import '@/assets/global.less'
+import '@/ElementUI/index'
 Vue.config.productionTip = false
-
 /* eslint-disable no-new */
+const a = 10
+console.log(a)
 new Vue({
   el: '#app',
   router,
