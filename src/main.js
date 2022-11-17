faf928b63ca5be1abada410877b7046a47fdb110
apprehen <3514392356@qq.com>
2022-11-17 23:43:34 +0800

    update

--- src/main.js
+++ src/main.js
@@ -5,8 +5,6 @@ import '@/assets/global.less'
 import '@/ElementUI/index'
 Vue.config.productionTip = false
 /* eslint-disable no-new */
-const a = 10
-console.log(a)
 new Vue({
   el: '#app',
   router,
